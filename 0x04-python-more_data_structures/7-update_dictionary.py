8f6d9deda929bd30bf99a08ee5598c9496cb175b
daadesina <daadesina1@gmail.com>
2024-01-25 00:28:55 +0100

    done

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-
-
-def update_dictionary(a_dictionary, key, value):
-    n_dic = {key: value}
-    a_dictionary.update(n_dic)
-    return a_dictionary
+def update_dictionary(a_dict, key, value):
+    """Returns a new dictionary with all values multiplied by 2"""
+    a_dict[key] = value
+    return a_dict
